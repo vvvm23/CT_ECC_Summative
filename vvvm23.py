c0738142a6f42d85529772e25a657fe1943933b0
Alexander McKinney <alexander.f.mckinney@durham.ac.uk>
2019-01-24 15:14:58 +0000

    placeholder functions

---
+++ vvvm23.py
@@ -0,0 +1,20 @@
+def message(a):
+    pass
+
+def hammingEncoder(m):
+    pass
+
+def hammingDecoder(v):
+    pass
+
+def messageFromCodeword(c):
+    pass
+
+def dataFromMessage(m):
+    pass
+
+def repetitionEncoder(m, n):
+    pass
+
+def repetitionDecoder(v):
+    pass
