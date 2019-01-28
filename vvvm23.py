d18bcddea6e2c80aa994f86e46201f641c99220b
Alexander McKinney <alexander.f.mckinney@durham.ac.uk>
2019-01-28 22:33:04 +0000

    commented code

--- vvvm23.py
+++ vvvm23.py
@@ -1,27 +1,28 @@
 import numpy as np
 import math as m
 
+# Converts from data to message format
 def message(a):
     l = len(a)
-    r_min = m.floor(m.log2(l)) + 1
+    r_min = m.floor(m.log2(l)) + 1 # Calculates minimum r. Could just start with r=2?
     if r_min < 2:
         r_min = 2
 
     r = r_min
-    while r + l > 2**r - r - 1:
+    while r + l > 2**r - r - 1: # Increments r until good value found
         r += 1
 
     l_b = decimalToVector(l, r)
-    return l_b + a + [0] * (2**r - 2*r - 1 - l)
+    return l_b + a + [0] * (2**r - 2*r - 1 - l) # Appends length vector to message padded by 0s
 
+# Converts message into hamming code
 def hammingEncoder(m):
     k = len(m)
-
     if k < 1:
         return []
 
     r = 2
-    while (1):
+    while (1): # Increments r until valid r found
         if k > 2**r - r - 1:
             r += 1
         elif k < 2**r - r - 1:
@@ -30,12 +31,14 @@ def hammingEncoder(m):
             break
 
     G = np.array(hammingGeneratorMatrix(r))
-    return (np.dot(m, G) % 2).tolist()
+    return (np.dot(m, G) % 2).tolist() # Calculates inner product between m and G
 
+# Checks for errors in hamming code and corrects if possible
+# Maybe change this to another method other than brute force?
 def hammingDecoder(v):
     k = len(v)
     r = 2
-    while 1:
+    while 1: # Increment r until valid r found
         if k > 2**r - 1:
             r += 1
         elif k < 2**r - 1:
@@ -43,22 +46,23 @@ def hammingDecoder(v):
         else:
             break
 
-    m_i = 0
+    m_i = 0 # Possible message index
     G = np.array(hammingGeneratorMatrix(r))
     while m_i < 2**k - 1:
-        m_b = decimalToVector(m_i, 2**r - r - 1)
-        m_c = np.dot(m_b, G) % 2
-        if hammingDistance(m_c, v) <= 1:
+        m_b = decimalToVector(m_i, 2**r - r - 1) # Gets binary vector of message
+        m_c = np.dot(m_b, G) % 2 # Calculates inner product with generator to get candidate
+        if hammingDistance(m_c, v) <= 1: # If candidate is within 1 distance away return candidate
             return m_c.tolist()
         else:
-            m_i += 1
+            m_i += 1 # ..else increment index
 
-    return []
+    return [] # If tried all possibilities return empty vector
 
+# Gets the message from the hamming code
 def messageFromCodeword(c):
     r = 2
     k = len(c)
-    while 1:
+    while 1: # Increments r until valid r found
         if k > 2**r - 1:
             r += 1
         elif k < 2**r - 1:
@@ -66,17 +70,19 @@ def messageFromCodeword(c):
         else:
             break
 
+    # Try and make this into a nice one liner?
     out = []
     for x in range(2**r - 1):
-        if not m.log2(x+1) == m.floor(m.log2(x+1)):
+        if not m.log2(x+1) == m.floor(m.log2(x+1)): # If not power of 2 append to output
             out.append(c[x])
 
     return out
 
+# Gets original data from message
 def dataFromMessage(m):
     r = 2
     k = len(m)
-    while 1:
+    while 1: # Increment r until valid r found
         if k > 2**r - r - 1:
             r += 1
         elif k < 2**r - r - 1:
@@ -84,33 +90,38 @@ def dataFromMessage(m):
         else:
             break
 
-    l_b = m[:r]
+    l_b = m[:r] # Slice message to get binary vector length
     l = 0
-    for i in range(r):
+    for i in range(r): # Get decimal length
         l += l_b[i] * 2**(r-i-1)
 
-    if sum(m[r+l:]) > 0 or l + r > k:
+    if sum(m[r+l:]) > 0 or l + r > k: 
+        # If none zero encountered after message or calculated l+r
+        #.. more than length return empty vector
         return []
 
-    return m[r:r+l]
+    return m[r:r+l] # Return slice of message from r to r+l -> [r, r+l)
 
-
+# Repetition encodes a message n times
 def repetitionEncoder(m, n):
+    # Get the outer product of m and repetition G matrix.
+    # Then flatten to obtain code vector
     return (np.outer(m, np.array(repetitionGeneratorMatrix(n))).flatten() % 2).tolist()
 
+# Decodes a repeated array into one bit
 def repetitionDecoder(v):
     # Does this need to decode vectors with multiple values in it?
     # Eg: 1100 = 10 rather than empty? 
     n = len(v)
     S = sum(v)
-    if S > n / 2:
+    if S > n / 2: # If sum is greater than half of length then it was 1
         return [1]
-    elif S < n / 2:
+    elif S < n / 2: # If sum is less than half of length then it was 0
         return [0]
-    else:
+    else: # If sum = half of length cannot determine
         return []
 
-
+# Generates an r-hamming generator matrix
 def hammingGeneratorMatrix(r):
     n = 2**r-1
     
@@ -147,16 +158,20 @@ def hammingGeneratorMatrix(r):
 
     return G
 
+# Generates hamming parity matrix of size r
 def parityGeneratorMatrix(r):
     H = []
-    for i in range(1, 2**r):
+    for i in range(1, 2**r): # Let rows be binary vectors from 1 to 2**r - 1
         H.append(decimalToVector(i, 3))
 
-    return np.array(H).T.tolist()
+    # Transform and return (or don't bother transforming to save time later)
+    return np.array(H).T.tolist() 
 
+# Creates repetition generator matrix
 def repetitionGeneratorMatrix(n):
-    return [1]*n
+    return [1]*n # Simply a vector of all 1s
 
+# Converts n decimal number into binary vector
 def decimalToVector(n,r): 
     v = []
     for s in range(r):
@@ -164,5 +179,8 @@ def decimalToVector(n,r):
         n //= 2
     return v
 
+# Calculates hamming distance between m and v
 def hammingDistance(m, v):
+    # Creates list of tuples m and v and applies xor
+    #..then sum resulting list to obtain distance
     return sum(list(map(lambda _: _[0] ^ _[1], list(zip(m,v)))))
